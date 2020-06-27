4c59cbc31cf9a370acadaa23ef10ea2ed170d83b
Abhishek <63309585+SweetSpecimen@users.noreply.github.com>
2020-06-27 17:55:21 +0530

    Add files via upload

---
+++ WT CS/myjs.js
@@ -0,0 +1,3 @@
+alert("Hi! This is abhishek from Big Paper Co.")
+var name = prompt("Enter your name: ")
+alert("Hello "+name+". We are glad to see you here.")
---
+++ WT CS/myjs2.js
@@ -0,0 +1,1 @@
+alert("BATCH 5: Abhishek(18) Yashwant(15) Jyotshna(26) and Mourya(48)")
